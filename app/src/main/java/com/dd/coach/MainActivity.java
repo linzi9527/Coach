3318884d6b1f3cee18f22bb6a5849c49e35984e0
linzi9527 <linzi_422109@sina.com>
2018-06-01 13:33:37 +0800

    第一次github提交项目创建

---
+++ app/src/main/java/com/dd/coach/MainActivity.java
@@ -0,0 +1,21 @@
+package com.dd.coach;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+*
+*描述：MainActivity
+*
+*撸码人：linzi
+*
+*日期：2018/6/1 13:22
+*
+*/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
