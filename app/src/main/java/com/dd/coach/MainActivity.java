25d4d4cad4c10cfdadfb74238a9d6b8ec070254b
linzi9527 <linzi_422109@sina.com>
2018-06-01 14:04:56 +0800

    添加页面控件

--- app/src/main/java/com/dd/coach/MainActivity.java
+++ app/src/main/java/com/dd/coach/MainActivity.java
@@ -1,7 +1,12 @@
 package com.dd.coach;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 /**
 *
 *描述：MainActivity
@@ -17,5 +22,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv=findViewById(R.id.tv);
+        tv.setText("我是动态内容activity...");
+        Button btn=findViewById(R.id.btn);
+        btn.setText("按钮点击+");
+        btn.setOnClickListener(new BtnClickListener());
+    }
+
+    class BtnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent=new Intent();
+            intent.putExtra("show","I can fly");
+           // intent.setClass(MainActivity.this,DodActivity.class);
+           // MainActivity.this.startActivity(intent);
+           // Boss b=new Boss();
+           // b.workOfBoss();
+        }
     }
 }
